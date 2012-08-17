5f81ef95e481a1b6e6a0a20527cf712ed7f957ab
Felipe Magno de Almeida <eu@felipemagno.com.br>
2012-08-17 10:59:43 -0300

    Added ior project

---
+++ ior/include/tecorb/ior/grammar/ior.hpp
@@ -0,0 +1,17 @@
+/* (c) Copyright 2012 Felipe Magno de Almeida
+ *
+ * Distributed under the Boost Software License, Version 1.0. (See
+ * accompanying file LICENSE_1_0.txt or copy at
+ * http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+#ifndef TECORB_IOR_GRAMMAR_IOR_HPP
+#define TECORB_IOR_GRAMMAR_IOR_HPP
+
+namespace tecorb { namespace ior { namespace grammar {
+
+
+
+} }
+
+#endif
