463a4214e667abaebaf9d158ba789df5e562e6b9
Felipe Magno de Almeida <eu@felipemagno.com.br>
2012-11-16 12:54:15 -0200

    Added asserts to tests

--- tests/struct_server.cpp
+++ tests/struct_server.cpp
@@ -21,6 +21,16 @@ struct struct_interface_impl : POA_struct_interface
   {
     std::cout << "struct_interface_impl::foo1 called" << std::endl;
     assert(!foo1_ && !foo2_ && !foo3_ && !foo4_);
+    assert(s.a1);
+    assert(!s.b1);
+    assert(s.c1);
+    assert(s.a2 == 'a');
+    assert(s.b2 == 'b');
+    assert(s.a3 == 2.0);
+    assert(s.a4 == 2.0f);
+    assert(s.a5 == 2);
+    assert(s.a6 == 'c');
+    assert(s.a7 == 2);
     foo1_ = true;
   }
 
@@ -28,6 +38,16 @@ struct struct_interface_impl : POA_struct_interface
   {
     std::cout << "struct_interface_impl::foo2 called" << std::endl;
     assert(foo1_ && !foo2_ && !foo3_ && !foo4_);
+    s.a1 = false;
+    s.b1 = true;
+    s.c1 = false;
+    s.a2 = 'c';
+    s.b2 = 'd';
+    s.a3 = 3.0;
+    s.a4 = 3.0f;
+    s.a5 = 3;
+    s.a6 = 'e';
+    s.a7 = 3;
     foo2_ = true;
   }
 
@@ -35,6 +55,28 @@ struct struct_interface_impl : POA_struct_interface
   {
     std::cout << "struct_interface_impl::foo3 called" << std::endl;
     assert(foo1_ && foo2_ && !foo3_ && !foo4_);
+    assert(s.a1);
+    assert(!s.b1);
+    assert(s.c1);
+    assert(s.a2 == 'a');
+    assert(s.b2 == 'b');
+    assert(s.a3 == 2.0);
+    assert(s.a4 == 2.0f);
+    assert(s.a5 == 2);
+    assert(s.a6 == 'c');
+    assert(s.a7 == 2);
+
+    s.a1 = false;
+    s.b1 = true;
+    s.c1 = false;
+    s.a2 = 'c';
+    s.b2 = 'd';
+    s.a3 = 3.0;
+    s.a4 = 3.0f;
+    s.a5 = 3;
+    s.a6 = 'e';
+    s.a7 = 3;
+
     foo3_ = true;
   }
 
@@ -44,7 +86,8 @@ struct struct_interface_impl : POA_struct_interface
     assert(foo1_ && foo2_ && foo3_ && !foo4_);
     foo4_ = true;
     orb->shutdown(true);
-    return some_struct();
+    some_struct r = {true, false, true, 'a', 'b', 2.0, 2.0f, 2, 'c', 2};
+    return r;
   }
 
   bool foo1_, foo2_, foo3_, foo4_;
