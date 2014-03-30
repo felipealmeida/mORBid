8d9364be5668b4e70941cff5aca32ffadcdf9e8a
Felipe Magno de Almeida <felipe@expertisesolutions.com.br>
2014-03-30 18:47:17 -0300

    Fixed oil client and server benchmark

--- orb/benchmarks/simplest/oil/client.lua
+++ orb/benchmarks/simplest/oil/client.lua
@@ -2,23 +2,38 @@
 --io.stdout:setvbuf("no")
 
 oil = require "oil"                                             -- Load OiL package
+--parsed = require "parsed"
 
-parsed = require "parsed"
+--parsed = require "parsed"
+
+ior_path = assert ( _G["MORBID_IOR_PATH"] or ...
+                  )
+
 
 oil.main(function()
-       local orb = oil.init()
+
+        --oil.verbose:level(5)
+        --oil.verbose.viewer.output = assert(io.open("client.log", "w"))
+        --oil.verbose.timed = true
+        --local TimeFormat = "%d/%m/%Y %H:%M:%S"
+        --oil.verbose:settimeformat(TimeFormat) -- inclui data e hora no log do OiL
+
+        local orb = oil.init()
 
         local array = require "table"
 
-        assert ( _G["MORBID_IOR_PATH"] )
+        --local array = require "table"
+        --local unpack = array.unpack or _G.unpack
+        --orb.TypeRepository.registry:register(unpack(parsed))
 	
-	simplest = orb:newproxy(assert(oil.readfrom(_G["MORBID_IOR_PATH"]))) -- Get proxy to object
+	simplest = orb:newproxy(assert(oil.readfrom(ior_path))) -- Get proxy to object
+        orb:loadidlfile('/home/felipe/dev/tecgraf/morbid/orb/benchmarks/simplest/simplest.idl')
 
         print "Starting"
         io.stdout:flush()
 
         local before = os.clock()	
-	for i = 1, 10000 do
+	for i = 1, 1000000 do
            simplest:foo() 
         end
 
--- orb/benchmarks/simplest/oil/server.lua
+++ orb/benchmarks/simplest/oil/server.lua
@@ -2,13 +2,21 @@
 --io.stdout:setvbuf("no")
 
 oil = require "oil"                                    -- Load OiL package
-parsed = require "parsed"
+--parsed = require "parsed"
 
 start = os.clock()
 
+ior_path = assert ( _G["MORBID_IOR_PATH"] or ... 
+                  )
+
+
 oil.main(function()
 
-        assert ( _G["MORBID_IOR_PATH"] )
+        --oil.verbose:level(2)
+        --oil.verbose.viewer.output = assert(io.open("server.log", "w"))
+        --oil.verbose.timed = true
+        --local TimeFormat = "%d/%m/%Y %H:%M:%S"
+        --oil.verbose:settimeformat(TimeFormat) -- inclui data e hora no log do OiL
 
         local times = 0;
 
@@ -16,18 +24,19 @@ oil.main(function()
 
 	local simplest = {  }      -- Get object implementation
 	function simplest:foo()
-           print( "foo " , times)
+           --print( "foo " , times)
            io.stdout:flush()
            times = times + 1
-           if times == 10000 then
+           if times == 1000000 then
              orb:shutdown()
            end
            return true
 	end
 	
-        local array = require "table"
-        local unpack = array.unpack or _G.unpack
-        orb.TypeRepository.registry:register(unpack(parsed))
+        --local array = require "table"
+        --local unpack = array.unpack or _G.unpack
+        --orb.TypeRepository.registry:register(unpack(parsed))
+        orb:loadidlfile('/home/felipe/dev/tecgraf/morbid/orb/benchmarks/simplest/simplest.idl')
 
 	simplest = orb:newservant(simplest, nil, "simplest")                  -- Create CORBA object
 
@@ -36,7 +45,7 @@ oil.main(function()
 
         print ("Initialization time is ", (os.clock()-start))
 
-	if not oil.writeto(_G["MORBID_IOR_PATH"], ref.."\n") then
+	if not oil.writeto(ior_path, ref.."\n") then
 		print(ref)
 	end
 
